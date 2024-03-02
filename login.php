cb6e688bf8d1cadf63ab32b4d62189f976829c24
SukhovKirill <160316848+SukhovKirill@users.noreply.github.com>
2024-03-02 14:20:43 +0400

    Update login.php

--- login.php
+++ login.php
@@ -19,8 +19,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
+    //Защита от sql-иньекций, без него не защищена
     $username = mysqli_real_escape_string($conn, $username);
-	$password = mysqli_real_escape_string($conn, $password);
+    $password = mysqli_real_escape_string($conn, $password);
 
     $sql = "SELECT * FROM users WHERE username='$username' AND password='$password'";
     $result = mysqli_query($conn,$sql);
